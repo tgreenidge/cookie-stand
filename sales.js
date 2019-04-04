2dbc3dd06ec725e5a554ddc607b5914a98e5d48a
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-04-04 00:23:36 -0700

    refactor to create event handler for form submit. Reorganized code to move helper functions that are not sales specific to app.js. Reorganized code in sales.js

--- sales.js
+++ sales.js
@@ -1,79 +1,11 @@
 'use strict';
 // This file contains data and javascript for sales.html page
+//makes calls to helper functions in app.js
 
-/************************************ helper functions **************************/
-// generates a random number between max and min inclusive
-var generateRandomNumber = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-// creates a dom element
-var createAnElement = function(ele) {
-  var element = document.createElement(ele);
-  return element;
-};
-
-//appends a child element to parent html element
-var appendElement = function(parent, child) {
-  parent.appendChild(child);
-};
-
-// add text content to an html element
-var addTextContent = function(ele, text){
-  ele.textContent = text;
-};
-
-// makes a table header of properties
-var makeTableColumnHeaders = function(table, tableHeaderArr, lastColHeader) {
-  // create a table row
-  var trEl = createAnElement('tr');
-  var thEl;
-
-  //make first column of table
-  thEl= createAnElement('th');
-  addTextContent(thEl, '');
-  appendElement(trEl, thEl);
-
-  // add column headings for table
-  for (var i = 0; i < tableHeaderArr.length; i++){
-    // add new header to the row
-    thEl= createAnElement('th');
-    addTextContent(thEl, tableHeaderArr[i]);
-    appendElement(trEl, thEl);
-  }
-
-  // make last column header of table
-  thEl= createAnElement('th');
-  addTextContent(thEl, lastColHeader);
-  appendElement(trEl, thEl);
-
-  // apprend row to the table
-  appendElement(table, trEl);
-};
-
-var makeTableColumnFooters = function(tableElement, tableData, lastCell) {
-  //make new row
-  var trEl = createAnElement('tr');
-  var tdEl = createAnElement('td');
-  addTextContent(tdEl, 'Totals');
-  appendElement(trEl, tdEl);
-
-  for( var k = 0; k < tableData.length; k++){
-    tdEl = createAnElement('td');
-    addTextContent(tdEl, tableData[k]);
-    appendElement(trEl, tdEl);
-  }
-
-  tdEl = createAnElement('td');
-  addTextContent(tdEl, lastCell);
-  appendElement(trEl, tdEl);
-  appendElement(salesTable, trEl);
-};
-
+/************************* store global variables *************************/
 // grab sales-info div on DOM for to append table
 var salesTable = document.getElementById('sales-table');
 
-/************************* store info and constructor *************************/
 // all locations for Salmon Cookies Business
 var allStoreLocations = [];
 
@@ -83,7 +15,55 @@ var allStoreTotalsByHour = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 // keeps tack of daily totals for all stores
 var totalDaySalesAcrossAllStores = 0;
 
-// Store constructor
+/******************************** Render Information to sales.html ****************/
+
+var updateSalesTable = function() {
+  if (salesTable.innerText === ''){
+    // make a column headers for sales data on new page load
+    makeTableColumnHeaders(salesTable, allStoreLocations[0]['storeHours'], 'Daily Location Total');
+
+    // display store data on tables
+    for (var j = 0; j < allStoreLocations.length; j++){
+      allStoreLocations[j].render();
+    }
+
+    makeTableColumnFooters(salesTable, allStoreTotalsByHour, totalDaySalesAcrossAllStores);
+  } else {
+    //delete last row of existing table (footer) 
+    var numRowsInTable = salesTable.rows.length;
+    salesTable.deleteRow(numRowsInTable - 1);
+
+    //insert new row to update (last element in allStoreLocations array)
+    allStoreLocations[allStoreLocations.length - 1].render();
+
+    //insert updated Footer
+    makeTableColumnFooters(salesTable, allStoreTotalsByHour, totalDaySalesAcrossAllStores);
+  }
+};
+
+/******************************** New Store Form Event Handler ****************/
+
+//when user enters data to create new store, all input in collected from form on submit, and sales table is updated
+var handleNewStoreSubmit = function(event) {
+  event.preventDefault();
+  var formData = event.target;
+  var newStoreLocationName = formData[1].value;
+  var newStoreMinCustomers = parseInt(formData[2].value);
+  var newStoreMaxCustomers = parseInt(formData[3].value);
+  var newStoreAverageCookieSale = parseFloat(formData[4].value);
+  var newStore = new SalmonCookiesStore(newStoreLocationName, newStoreMinCustomers, newStoreMaxCustomers, newStoreAverageCookieSale);
+  updateSalesTable();
+  newStoreForm.reset();
+};
+
+/******************************** New Store Form Event Listeners ****************/
+
+//when user enters data to create new store, all input in collected from form on submit, and sales table is updated
+var newStoreForm = document.getElementById('new-store-form');
+newStoreForm.addEventListener('submit', handleNewStoreSubmit);
+
+
+/******************************** Store Constructor ****************/
 function SalmonCookiesStore(locationName, minCust, maxCust, avgCookieSale) {
   this.locationName = locationName;
   this.minCust = minCust;
@@ -158,6 +138,7 @@ function SalmonCookiesStore(locationName, minCust, maxCust, avgCookieSale) {
   allStoreLocations.push(this);
 }
 
+/***************************** when user refreshes page, create simulated data and update table *************/
 // create stores
 var firstAndPike = new SalmonCookiesStore('First and Pike', 23, 65, 6.3);
 var seaTacAirport = new SalmonCookiesStore('SeaTac Airport', 3, 24, 1.2);
@@ -165,49 +146,4 @@ var seattleCenter = new SalmonCookiesStore('Seattle Center', 11, 38, 3.7);
 var capitolHill = new SalmonCookiesStore('Capitol Hill', 20, 38, 2.3);
 var alki = new SalmonCookiesStore('Alki', 2, 16, 4.6);
 
-
-/******************************** Render Information to sales.html ****************/
-
-var updateSalesTable = function() {
-  if (salesTable.innerText === ''){
-    // make a column headers for sales data on new page load
-    makeTableColumnHeaders(salesTable, allStoreLocations[0]['storeHours'], 'Daily Location Total');
-
-    // display store data on tables
-    for (var j = 0; j < allStoreLocations.length; j++){
-      allStoreLocations[j].render();
-    }
-
-    makeTableColumnFooters(salesTable, allStoreTotalsByHour, totalDaySalesAcrossAllStores);
-  } else {
-    //delete last row of existing table (footer) 
-    var numRowsInTable = salesTable.rows.length;
-    salesTable.deleteRow[numRowsInTable];
-
-    //insert new row to update (last element in allStoreLocations array)
-    allStoreLocations[allStoreLocations.length - 1].render();
-
-    //insert updated Footer
-    makeTableColumnFooters(salesTable, allStoreTotalsByHour, totalDaySalesAcrossAllStores);
-  }
-};
-
-
-/******************************** New Store Form Event Listeners ****************/
-
-//when user enters data to create new store, all input in collected from form on submit, and sales table is updated
-var newStoreForm = document.getElementById('new-store-form');
-newStoreForm.addEventListener('submit', function(event){
-  event.preventDefault();
-  var formData = event.target;
-  var newStoreLocationName = formData[1].value;
-  var newStoreMinCustomers = parseInt(formData[2].value);
-  var newStoreMaxCustomers = parseInt(formData[3].value);
-  var newStoreAverageCookieSale = parseFloat(formData[4].value);
-  var newStore = new SalmonCookiesStore(newStoreLocationName, newStoreMinCustomers, newStoreMaxCustomers, newStoreAverageCookieSale);
-  updateSalesTable();
-  newStoreForm.reset();
-});
-
-//when user refreshes page, update table
 updateSalesTable();
